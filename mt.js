41888f03f7e120d77007144a6f1bfbb27af7f513
maxi3219 <maxi3219@mail.ru>
2025-11-13 22:38:10 +0300

    mt.js

--- mt.js
+++ mt.js
@@ -123,8 +123,9 @@
         div.id = 'MRELOAD';
         div.className = 'head__action selector m-reload-screen';
         div.innerHTML = `
-            <svg viewBox="0 0 24 24">
-                <path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z"
+                      fill="#ffffff"></path>
             </svg>
         `;
         div.addEventListener('click', () => location.reload());
@@ -153,7 +154,7 @@
             app.plugins.add({
                 id: plugin_id_menu,
                 name: plugin_name_menu,
-                version: '5.8',
+                version: '5.9',
                 author: 'maxi3219',
                 description: 'Скруглённое меню + тёмный фон + фикс кнопок + кнопка перезагрузки',
                 init: initMenuPlugin
@@ -215,10 +216,3 @@
                 description: 'Окрашивает число после "Раздают:" без свечения',
                 init: startObserver
             });
-        } else {
-            startObserver();
-        }
-    }
-
-    registerColor();
-})();
