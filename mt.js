8196dd05beaad3bf2505e4c89fb0ea7bb29317af
maxi3219 <maxi3219@mail.ru>
2025-11-13 19:58:31 +0300

    mt.js

--- mt.js
+++ mt.js
@@ -22,7 +22,7 @@
                     width: 35% !important;
                     max-height: calc(100vh - 2em) !important;
                     overflow-y: auto !important;
-                    background: rgba(54,54,54,.959) !important;
+                    background: rgba(54,54,54,0.98) !important; /* ← менее прозрачный фон */
                     border-radius: 1.2em !important;
                     box-shadow: 0 8px 24px rgba(0, 0, 0, 0.8) !important;
                     padding: 0.5em !important;
